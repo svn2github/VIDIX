98f962fa350e718148153eed7febdd1b4a8f50b0
nickols_k <nickols_k@84fa5670-3129-0410-a1ff-e783189bc518>
2002-04-22 07:37:53 +0000

    sync with mplayer
    git-svn-id: https://svn.code.sf.net/p/vidix/code/trunk@16 84fa5670-3129-0410-a1ff-e783189bc518

--- vidix/vidix/drivers/mach64_vid.c
+++ vidix/vidix/drivers/mach64_vid.c
@@ -20,6 +20,7 @@
 #include "../../libdha/pci_names.h"
 
 #include "mach64.h"
+#include "../../version.h"
 
 #define UNUSED(x) ((void)(x)) /**< Removes warning about unused arguments */
 
@@ -144,11 +145,17 @@ static __inline__ int ATIGetMach64LCDReg(int _Index)
 static __inline__ uint32_t INPLL(uint32_t addr)
 {
     uint32_t res;
-
+    uint32_t in;
+    
+    /* preserve unknown bits */
+    in= INREG8(CLOCK_CNTL + 1);
+    if(__verbose>0)printf("[mach64] pll: %X\n", in);
+    in &= ~((PLL_WR_EN | PLL_ADDR)>>8); //clean some stuff
+    
     /* write addr byte */
-    OUTREG8(CLOCK_CNTL + 1, (addr << 2));
+    OUTREG8(CLOCK_CNTL + 1, in | (addr << 2));
     /* read the register value */
-    res = INREG(CLOCK_CNTL + 2);
+    res = INREG8(CLOCK_CNTL + 2);
     return res;
 }
 
@@ -256,13 +263,15 @@ static uint32_t mach64_get_yres( void )
 static int mach64_get_vert_stretch(void)
 {
     int lcd_index;
-    int lcd_gen_ctrl;
     int vert_stretching;
     int ext_vert_stretch;
     int ret;
     int yres= mach64_get_yres();
 
-    if(!supports_lcd_v_stretch) return 1<<16;
+    if(!supports_lcd_v_stretch){
+        if(__verbose>0) printf("[mach64] vertical stretching not supported\n");
+        return 1<<16;
+    }
 
     lcd_index= INREG(LCD_INDEX);
     
@@ -442,6 +451,8 @@ int vixInit(void)
     printf("[mach64] Driver was not probed but is being initializing\n");
     return EINTR;
   }
+  if(__verbose>0) printf("[mach64] version %s\n", VERSION);
+  
   if((mach64_mmio_base = map_phys_mem(pci_info.base2,0x4000))==(void *)-1) return ENOMEM;
   mach64_wait_for_idle();
   mach64_ram_size = INREG(MEM_CNTL) & CTL_MEM_SIZEB;
@@ -734,7 +745,7 @@ static int mach64_vid_init_video( vidix_playback_t *config )
     v_inc>>=4; // convert 16.16 -> 20.12
     v_inc/= dest_h;
     
-    h_inc = (src_w << 12) / dest_w;
+    h_inc = (src_w << (12+ecp)) / dest_w;
     /* keep everything in 16.16 */
     config->offsets[0] = 0;
     for(i=1; i<config->num_frames; i++)
