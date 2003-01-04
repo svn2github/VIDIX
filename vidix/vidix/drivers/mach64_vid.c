82cb7e9f76eca02e689d8422c19bcb383053659d
nickols_k <nickols_k@84fa5670-3129-0410-a1ff-e783189bc518>
2003-01-04 08:38:14 +0000

    some fixes
    git-svn-id: https://svn.code.sf.net/p/vidix/code/trunk@121 84fa5670-3129-0410-a1ff-e783189bc518

--- vidix/vidix/drivers/mach64_vid.c
+++ vidix/vidix/drivers/mach64_vid.c
@@ -175,8 +175,11 @@ static video_registers_t vregs[] =
 
 #define INREG8(addr)		GETREG(uint8_t,(uint32_t)mach64_mmio_base,((addr)^0x100)<<2)
 #define OUTREG8(addr,val)	SETREG(uint8_t,(uint32_t)mach64_mmio_base,((addr)^0x100)<<2,val)
-#define INREG(addr)		le2me_32(GETREG(uint32_t,(uint32_t)mach64_mmio_base,((addr)^0x100)<<2))
-#define OUTREG(addr,val)	SETREG(uint32_t,(uint32_t)mach64_mmio_base,((addr)^0x100)<<2,le2me_32(val))
+static inline uint32_t INREG (uint32_t addr) {
+    uint32_t tmp = GETREG(uint32_t,(uint32_t)mach64_mmio_base,((addr)^0x100)<<2);
+    return le2me_32(tmp);
+}
+#define OUTREG(addr,val)	SETREG(uint32_t,(uint32_t)mach64_mmio_base,((addr)^0x100)<<2,me2le_32(val))
 
 #define OUTREGP(addr,val,mask)  					\
 	do {								\
