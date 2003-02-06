4cf417e2cee593f318578e124db215df0dfd2138
nickols_k <nickols_k@84fa5670-3129-0410-a1ff-e783189bc518>
2003-02-06 18:55:52 +0000

    fixed compilation on sparcs by rezso@rdsor.ro
    git-svn-id: https://svn.code.sf.net/p/vidix/code/trunk@142 84fa5670-3129-0410-a1ff-e783189bc518

--- vidix/libdha/sysdep/pci_linux.c
+++ vidix/libdha/sysdep/pci_linux.c
@@ -20,6 +20,10 @@ int dhahelper_initialized = 0;
 int dhahelper_fd = 0;
 #endif
 
+#ifdef __sparc__
+#define iopl(x) (0)
+#endif
+
 static __inline__ int enable_os_io(void)
 {
 #ifdef CONFIG_DHAHELPER
