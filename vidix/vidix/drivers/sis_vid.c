e3baccaa35195e4170ad9ed721c93e0b16834637
gxben <gxben@84fa5670-3129-0410-a1ff-e783189bc518>
2007-02-11 22:07:54 +0000

    screen_height calculation was using an uninitialized variable, see http://svn.mplayerhq.hu/mplayer?view=rev&revision=22043
    git-svn-id: https://svn.code.sf.net/p/vidix/code/trunk@247 84fa5670-3129-0410-a1ff-e783189bc518

--- vidix/vidix/drivers/sis_vid.c
+++ vidix/vidix/drivers/sis_vid.c
@@ -368,7 +368,7 @@ int vixInit(const char *args __attribute__ ((unused)))
     inSISIDXREG(SISCR, 0x07, cr_data2);
     sis_screen_height =
 	((cr_data & 0xff) | ((uint16_t) (cr_data2 & 0x02) << 7) |
-	 ((uint16_t) (cr_data2 & 0x40) << 3) | ((uint16_t) (sr_data & 0x02)
+	 ((uint16_t) (cr_data2 & 0x40) << 3) | ((uint16_t) (cr_data & 0x02)
 						<< 9)) + 1;
 
     inSISIDXREG(SISSR, 0x0b, sr_data);
