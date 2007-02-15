b02116a32c19b048a95d9efd69dd40df731e9ea4
gxben <gxben@84fa5670-3129-0410-a1ff-e783189bc518>
2007-02-15 22:38:11 +0000

    hum ... unsigned int with -1 value
    git-svn-id: https://svn.code.sf.net/p/vidix/code/trunk@268 84fa5670-3129-0410-a1ff-e783189bc518

--- vidix/vidix/drivers/sis_defs.h
+++ vidix/vidix/drivers/sis_defs.h
@@ -98,7 +98,7 @@ extern unsigned short sis_iobase;
 extern unsigned int sis_vga_engine;
 extern unsigned int sis_vbflags;
 extern unsigned int sis_overlay_on_crt1;
-extern unsigned int sis_crt1_off;
+extern int sis_crt1_off;
 extern unsigned int sis_detected_crt2_devices;
 extern unsigned int sis_force_crt2_type;
 extern unsigned int sis_device_id;
